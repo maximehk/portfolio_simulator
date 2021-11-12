0eb34e8e8139a0f96cf99a976645f676b9e084b5
Maxime Hacker <maximeh@google.com>
2021-11-12 10:27:22 +0100

    updated the project structure

--- setup.py
+++ setup.py
@@ -5,19 +5,20 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(
-     name='portfolio simulator',  
-     version='0.1',
-     packages=['portfolio_simulator'] ,
-     author="Maxime",
-     author_email="",
-     description="A library that helps you run financial simulations using your financial assets as input.",
-     long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/maximehk/portfolio_simulator",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
- )
+        name='portfolio simulator',  
+        version='0.1',
+        author="Maxime",
+        author_email="",
+        description="A library that helps you run financial simulations using your financial assets as input.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/maximehk/portfolio_simulator",
+        package_dir={"": "src"},
+        packages=setuptools.find_packages(where="src"),
+        python_requires=">=3.6",
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            ],
+        )
